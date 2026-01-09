e934c49ab246aa12c846a6062ea5c290b037f466
suzannefinley <150373138+suzannefinley@users.noreply.github.com>
2026-01-08 18:06:39 -0600

    fixing footer logo link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,8 @@
+import {
+  SERVER_LOGO_URL,
+  MAIN_APP_URL,
+  COMPANY_NAME
+} from '@/lib/constants';
 import Image from 'next/image';
 
 const Footer = () => {
@@ -7,8 +12,7 @@ const Footer = () => {
     <footer className="border-t bg-gray-200 text-gray-700 flex justify-center ">
       <div className="grid grid-cols-1 my-10 ">
         <div className="flex justify-center ">
-          © {currentYear} {process.env.COMPANY_NAME}. All Rights
-          reserved.
+          © {currentYear} {COMPANY_NAME}. All Rights reserved.
         </div>
         <div className="flex justify-center mb-5">
           <a
@@ -30,10 +34,10 @@ const Footer = () => {
             Get Your Own dgResume Today!
             <span className="flex justify-center mt-2 mx-2">
               <Image
-                src="/images/logo.jpg"
+                src={SERVER_LOGO_URL}
                 width={160}
                 height={200}
-                alt={`${process.env.MAIN_APP_URL} logo`}
+                alt={`${MAIN_APP_URL} logo`}
                 priority={true}
                 className="rounded-xl shadow-md dark:shadow-gray-100"
               />
